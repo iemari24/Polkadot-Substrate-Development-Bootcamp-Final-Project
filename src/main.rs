5493e85e1591c0c514461fb82067ee3de1330857
emrey <emreylmzcn2@gmail.com>
2023-07-28 00:10:59 +0300

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 fn main()
 {
-    print!("hello");
+    let number = -4.0;
+    let sqrt = find_sqr(number);
+
+    match sqrt {
+        Some(_value) => println!("a"),
+        None => println!("b"),
+    }
+}
+
+
+fn find_sqr(number: f64) -> Option<f64> {
+
+    if number >= 0.0 {
+        Some(number.sqrt())
+    }
+    else {
+        None
+    }
 }
