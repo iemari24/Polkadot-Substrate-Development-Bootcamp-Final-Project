446bb0d802aaec08785870c7143e4a2862421819
emrey <emreylmzcn2@gmail.com>
2023-07-28 00:13:31 +0300

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ fn main()
     let string1 = String::from("emre");
     let string2 = String::from("yilmazcan");
     
-    let _concatenated_string: String = concatenate_strings(string1, string2);
-    print!("{}", _concatenated_string);
+    let concatenated_string: String = concatenate_strings(string1, string2);
+    println!("{}", concatenated_string);
 }
 
 fn concatenate_strings(a: String, b: String) -> String
 {
-    let mut result = String::from("");
+    let mut result = String::new();
     result.push_str(&a);
     result.push_str(&b);
 
